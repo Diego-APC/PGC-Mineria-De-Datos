a6246c049b0cb58eb48f1f90ccb438e0332fadfe
Diego Pinzon <dalexanderpinzon@ucundinamarca.edu.co>
2026-04-08 12:02:46 -0500

    Base del proyecto

---
+++ app.py
@@ -0,0 +1,20 @@
+
+from flask import Flask, render_template
+
+import Clustering
+
+app = Flask(__name__)
+
+@app.route("/")
+
+def home():
+     return "hello World"
+
+@app.route("/PGC/")
+def pgc():
+     return render_template("index.html")
+
+@app.route("/Cluster/")
+def cluster():
+     info = Clustering.RealizarClustering()
+     return render_template("ClusterResults.html", resultados = info["resumenCluster"])
